3d46eb7258e2b52dea278779ec9c88558e2e795e
ashleykcheng <ashleykcheng@gmail.com>
2023-12-21 20:17:43 -0800

    Initial Commit

---
+++ ChewCLA/ChewCLAApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChewCLAApp.swift
+//  ChewCLA
+//
+//  Created by Ashley on 12/21/23.
+//
+
+import SwiftUI
+
+@main
+struct ChewCLAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
